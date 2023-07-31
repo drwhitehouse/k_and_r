8aafcaa16b4d0db9b2defa86c47e81bd7104d1a3
Dan Whitehouse <drw@dim-sum.org>
2023-07-31 09:54:34 +0100

    first commit

---
+++ hello/hello.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cstuff
+//
+//  Created by Dan Whitehouse on 28/07/2023.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
