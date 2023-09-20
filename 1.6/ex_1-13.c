563d3f8b0ac80bbfe98f79a71258e68ec6783008
Dan Whitehouse <drw@dim-sum.org>
2023-09-20 17:12:34 +0100

    keep on pushing

---
+++ 1.6/ex_1-13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define BUFFER 20
+
+int main()
+{
+    int c, inspace, count, histogram[BUFFER];
+    inspace = 1;
+    count = 0;
+    for (int i = 0; i < BUFFER; ++i)
+        histogram[i] = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n') {
+            if (inspace == 0) {
+                inspace = 1;
+                putchar('\n');
+            }
+        } else {
+            inspace = 0;
+            ++count;
+        }
+        if (inspace ==1) {
+            ++histogram[count];
+            count = 0;
+        }
+    }
+    for (int i = 0; i < BUFFER; ++i)
+        printf("%d\n", histogram[i]);
+}
