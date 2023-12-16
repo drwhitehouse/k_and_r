1d9d19b4da67091e35afcbd7d50d78d7619cd1fc
Dan Whitehouse <drw@dim-sum.org>
2023-12-16 17:46:40 +0000

    Saving progress

--- 1.9/ex_1-18.c
+++ 1.9/ex_1-18.c
@@ -2,27 +2,27 @@
 #define MAXLINE 1000 /* Max input line size */
 
 int my_getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
-/* remove trailing blanks, tabs, blank lines and print lines */
+/* print lines removing trailing spaces, tabs and removing entirely blank lines*/
 
 int main()
 {
     int len;                /* current line length */
-    int max;                /* max length so far */
     char line[MAXLINE];     /* current input length */
-    char longest[MAXLINE];  /* longest line saved here */
+    char lineout[MAXLINE];  /* output line saved here */
     int c;                  /* temp var for very long line reading */
 
-    max = 0;
     while ((len = my_getline(line, MAXLINE)) > 0) {
         if (line[len-1] != '\n') {
             while((c=getchar()) !=EOF && c!='\n')
                 ++len; 
         } 
-        if (len > 80) {
-            printf("%s", line);
-        }
+        printf("%d\n", len);
+        copy(lineout, line);
+        printf("%s", lineout);
     }    
+    return 0;
 }
 
 /* my_getline: read a line into s, return length */
@@ -38,4 +38,18 @@ int my_getline(char s[], int lim)
     }
     s[i] = '\0';
     return i;
+}
+
+/* copy: copy 'from' into 'to' - assume 'to' is big enough */
+
+/* 'to[]' and 'from[]' are pointers to the first element of the arrays that are passed to this function.
+   i.e. 'to[]' points to 'lineout', and 'from[]' points to 'line'
+   printing the values using a '%p' format string should confirm this */
+
+void copy(char to[], char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
 }
